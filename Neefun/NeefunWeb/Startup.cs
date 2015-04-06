eb6fca203e29a30708ba06997e4fa958b90c42eb
hanzzhang <hanz@microsoft.com>
2015-04-06 13:13:25 -0700

    add NeefunWeb Web Role Project

---
+++ Neefun/NeefunWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NeefunWeb.Startup))]
+namespace NeefunWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
