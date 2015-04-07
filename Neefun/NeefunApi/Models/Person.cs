e4731cce2e4e03c6090fe9ba32fbd1311f8ac9db
hanzzhang <hanz@microsoft.com>
2015-04-07 16:56:57 -0700

    add Person model and Mongo data access

---
+++ Neefun/NeefunApi/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Bson;
+
+namespace NeefunApi.Models
+{
+    public class Person
+    {
+        [BsonId(IdGenerator = typeof(CombGuidGenerator))]
+        public Guid Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("CreatedDate")]
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
---
+++ Neefun/NeefunApi/Mongo/Mongo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NeefunApi.Models;
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace NeefunApi.Mongo
+{
+    public class Mongo
+    {
+        private static string connectionString = "mongodb://hanzvm2.cloudapp.net";
+        private static string dbName = "Person";
+        private static string collectionName = "PersonCollection";
+        private static IMongoDatabase db = null;
+        static Mongo()
+        {
+            IMongoClient mongoClient = new MongoClient(connectionString);
+            db = mongoClient.GetDatabase(dbName);
+        }
+        public async Task<List<Person>> GetAllPersonAsync()
+        {
+            IMongoCollection<Person> collection = db.GetCollection<Person>(collectionName);
+            return await collection.Find<Person>(x => x.Name != null).ToListAsync();
+        }
+
+        // Creates a Person and inserts it into the collection in MongoDB.
+        public async Task CreatePersonAsync(Person Person)
+        {
+            IMongoCollection<Person> collection = db.GetCollection<Person>(collectionName);
+            await collection.InsertOneAsync(Person);
+        }
+    }
+}
