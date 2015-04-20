328d8aa28bb7ee17ec4659103bfea921f304d8a1
hanzzhang <hanz@microsoft.com>
2015-04-19 19:49:11 -0700

    add swim program

---
+++ Neefun/NeefunWeb/Controllers/SwimController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NeefunWeb.Controllers
+{
+    public class SwimController : Controller
+    {
+        // GET: Swim
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SummerTeam()
+        {
+            return View();
+        }
+        public ActionResult SpringTeam()
+        {
+            return View();
+        }
+        public ActionResult GroupLesson()
+        {
+            return View();
+        }
+    }
+}
