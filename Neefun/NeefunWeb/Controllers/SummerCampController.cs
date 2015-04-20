16715c643e4e952c512753ba5402fb842b36dc33
hanzzhang <hanz@microsoft.com>
2015-04-19 20:26:27 -0700

    add SummerCamp Controller

---
+++ Neefun/NeefunWeb/Controllers/SummerCampController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NeefunWeb.Controllers
+{
+    public class SummerCampController : Controller
+    {
+        // GET: SummerCamp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
