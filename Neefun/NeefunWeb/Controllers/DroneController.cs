328ccf814f8e01c6a2beda2d61bb33887a04fb45
hanzzhang <hanz@microsoft.com>
2015-04-29 20:27:12 -0700

    add Drone controller and view

---
+++ Neefun/NeefunWeb/Controllers/DroneController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NeefunWeb.Controllers
+{
+    public class DroneController : Controller
+    {
+        // GET: Drone
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
