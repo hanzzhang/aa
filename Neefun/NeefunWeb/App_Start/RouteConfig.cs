8a7683e6bf3e270f125cc71beb6c510d0c252156
Hanz Zhang <hanz@microsoft.com>
2015-10-02 09:01:02 -0700

    make neefun.com/swim the default page

--- Neefun/NeefunWeb/App_Start/RouteConfig.cs
+++ Neefun/NeefunWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NeefunWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Swim", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
